95e769e77dbc26029a11c8b2f3c379ab62089880
Piotr Kafel <peterkaf132@gmail.com>
2024-04-04 10:33:30 +0200

    switched out deprecated function data_generator

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-# Import packages
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import os
+import tensorflow as tf
 from tensorflow.keras.applications import DenseNet121
 from tensorflow.keras.applications.densenet import preprocess_input
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
@@ -16,6 +16,7 @@ test_dir = 'dataset/test'
 # Define parameters
 IMG_SIZE = (224, 224)
 BATCH_SIZE = 16
+num_classes = len(os.listdir(train_dir))
 
 # Define logs directory
 log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
@@ -23,34 +24,45 @@ log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 # Define checkpoints directory
 checkpoints_dir = "checkpoints"
 
-# Data generators - preprocess_input is now used here
-train_datagen = ImageDataGenerator(rotation_range=20,
-                                   width_shift_range=0.2,
-                                   height_shift_range=0.2,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
-                                   horizontal_flip=True,
-                                   fill_mode='nearest',
-                                   preprocessing_function=preprocess_input)
 
-validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
-test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+# Define preprocessing function
+def preprocessing(image, label):
+    image = preprocess_input(image)
+    return image, label
 
-train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    target_size=IMG_SIZE,
-                                                    batch_size=BATCH_SIZE,
-                                                    class_mode='categorical')
 
-validation_generator = validation_datagen.flow_from_directory(validation_dir,
-                                                              target_size=IMG_SIZE,
-                                                              batch_size=BATCH_SIZE,
-                                                              class_mode='categorical')
+# Load and preprocess the training dataset
+train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    train_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode='categorical'
+)
 
-test_generator = test_datagen.flow_from_directory(test_dir,
-                                                  target_size=IMG_SIZE,
-                                                  batch_size=BATCH_SIZE,
-                                                  class_mode='categorical',
-                                                  shuffle=False)
+
+# Load and preprocess the datasets
+validation_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    validation_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode='categorical'
+)
+
+# Load and preprocess the test dataset
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode='categorical',
+    shuffle=False
+)
+
+# Apply preprocessing to the training dataset
+train_dataset = train_dataset.map(preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
+# Apply preprocessing to the validation dataset
+validation_dataset = validation_dataset.map(preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
+# Apply preprocessing to the test dataset
+test_dataset = test_dataset.map(preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
 
 # Load the DenseNet model
 base_model = DenseNet121(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
@@ -62,17 +74,13 @@ base_model.trainable = False
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
 x = Dense(1024, activation='relu')(x)
-predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+predictions = Dense(num_classes, activation='softmax')(x)  # Use num_classes here
 
 # Final model
 model = Model(inputs=base_model.input, outputs=predictions)
 
-# See the layout
-model.summary()
-
 # Compile the model
 model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-
 
 # Define callbacks
 tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
@@ -88,15 +96,13 @@ early_stopping_callback = EarlyStopping(monitor="val_loss",
                                         restore_best_weights=True)
 
 # Train the model with TensorBoard callback
-history = model.fit(train_generator,
-                    steps_per_epoch=len(train_generator),
+history = model.fit(train_dataset,
                     epochs=20,
-                    validation_data=validation_generator,
-                    validation_steps=len(validation_generator),
+                    validation_data=validation_dataset,
                     callbacks=[tensorboard_callback, checkpoint_callback, early_stopping_callback])
 
 # Evaluate the model
-test_loss, test_acc = model.evaluate(test_generator, steps=len(test_generator))
+test_loss, test_acc = model.evaluate(test_dataset)
 print(f'Test accuracy: {test_acc}, Test loss: {test_loss}')
 
 # Save the model in TensorFlow SavedModel format
