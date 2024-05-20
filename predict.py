f8e3ba21e69888984c1cbe6825ff6c42eacd2412
Piotr Kafel <peterkaf132@gmail.com>
2024-05-20 16:31:18 +0200

    Trained model, added model predict script, logs and model data. ranamed test.py -> train.py to avoid confusion

---
+++ predict.py
@@ -0,0 +1,60 @@
+# Import packages
+import os
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications import DenseNet121
+from tensorflow.keras.applications.densenet import preprocess_input
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+from tensorflow.keras.optimizers import Adam
+
+# Define parameters
+IMG_SIZE = (224, 224)
+BATCH_SIZE = 16
+model_weights = 'trained_models/model.weights.h5'
+test_dir = 'dataset/test'
+num_classes = len(os.listdir(test_dir))
+
+# Load and preprocess the test dataset
+
+
+def preprocessing(image, label):
+    image = preprocess_input(image)
+    return image, label
+
+
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    test_dir,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode='int',
+    shuffle=False
+)
+# Apply preprocessing to the test dataset
+test_dataset = test_dataset.map(preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
+
+# Load weights
+# Load the DenseNet model
+base_model = DenseNet121(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+base_model.trainable = False
+
+# Add custom layers
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+predictions = Dense(num_classes, activation='softmax')(x)
+
+# Final model
+model = Model(inputs=base_model.input, outputs=predictions)
+
+
+model.compile(optimizer=Adam(), loss='sparse_categorical_crossentropy',
+              metrics=['sparse_categorical_accuracy'])
+
+print(f"Weights loaded from : {model_weights}")
+model.load_weights(model_weights)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_dataset)
+print(f'Test accuracy: {test_acc}, Test loss: {test_loss}')
