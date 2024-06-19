b766aac5148a61f41d89ea5b789685a9bbd01fa6
Piotr Kafel <peterkaf132@gmail.com>
2024-06-19 15:27:45 +0200

    Added script for single image prediction

---
+++ single_img_pred.py
@@ -0,0 +1,15 @@
+from config import *
+import tensorflow as tf
+
+# Replace 'path_to_your_image.jpg' with the actual path to the image you want to predict
+image_path = '/home/peter/PycharmProjects/card_classification/dataset/test/ABBOTTS BABBLER/155.jpg'
+
+test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    TEST_DIR,
+    image_size=IMG_SIZE,
+    batch_size=BATCH_SIZE,
+    label_mode='int',
+    shuffle=False
+)
+
+predict_image(image_path, test_dataset)
